e1aa336bbd4019c4fec1586a15ee844f2ad33be9
James Chapman <james@paymenteconomics.com>
2023-05-13 00:17:31 -0400

    Update get_exchange_info.R
    Initial version that deals with data quality issues in the API.

--- R/get_exchange_info.R
+++ R/get_exchange_info.R
@@ -3,35 +3,16 @@ get_exchange_info <- function(exchange_id = 2, api_key = Sys.getenv("CMC_API_KEY
               "id" = exchange_id,
               "api_key" = api_key)
   x <- do.call(call_cmc_api, tmp)
+  rep_val <- \(x) ifelse(is.null(x),"NA",x)
+  rep_valn <- \(x) ifelse(is.null(x),NA,x)
+  rep_vall <- \(x) ifelse(is.null(x),list(NA),x)
+  the_return <- tibble::as_tibble(list("id" = x[[1]]$id,
+                               "name" = x[[1]]$name,
+                               "desc" = rep_val(x[[1]]$description),
+                                "launch_date" = rep_val(x[[1]]$date_launched),
+                                "weekly_visits" = rep_valn(x[[1]]$weekly_visits),
+                                "spot_volume_usd" = rep_valn(x[[1]]$spot_volume_usd),
+                                "fiats" = rep_vall(x[1]$fiats)))
 
-
-  # We run this script in a loop since it is a paginated endpoint
-  # The default is 10k entries. As long as the number of entries
-  # returned is 10K we reset the offset and run the query again
-  the_return <- NULL
-  finished <- FALSE
-  start <- 1
-  while(!finished){
-    tmp <- list("path" = "/v1/exchange/map",
-                listing_status = "active,inactive",
-                "api_key" = api_key,
-                "start" = start)
-    x <- do.call(call_cmc_api, tmp)
-    # Below assumes we are only pulling USD quotes. This will break if
-    # more quotes are used in conversion
-    x <- x |>
-      jsonlite::flatten() |> #flatten recursive JSON structures
-      tibble::as_tibble() |>
-      dplyr::select_all(~gsub("\\.","_",.)) #Replace . with _ in col names
-    the_return <- the_return |> dplyr::bind_rows(x)
-    finished <- nrow(x) < 10000
-    if (!finished){
-      start <- start + 10000
-    }
-  }
-  # clean up the data a bit
-  the_return <- the_return |> dplyr::mutate(
-    first_historical_data = lubridate::ymd_hms(first_historical_data),
-    last_historical_data = lubridate::ymd_hms(last_historical_data))
   return(the_return)
 }
